734a65f8bcb4423f5cca0474f88427aa2d79dce5
Bojan Kuzmanović <bojank@live.co.uk>
2017-12-16 21:05:55 +0100

    Popravljeni konflikti sa osi.h

--- Projekat/Projekat/osi.h
+++ Projekat/Projekat/osi.h
@@ -6,18 +6,9 @@ typedef struct korisnik
 	int tip;
 	char ime[16], prezime[21];
 } KORISNIK;
-typedef struct artikal
-{
-	char* naziv[16], sifra[16];
-	double kolicina, cijena, ukupno;
-}ARTIKAL;
-typedef struct racun
-{
-	char nazivKupca[16],datum[11];
-	ARTIKAL* nizA;
-	struct racun* next;
-}RACUN;
-RACUN* glava;
+
+
+
 int prijava(KORISNIK*, int);
 int search(KORISNIK*, int, KORISNIK*, char*, char*);
 void registracija(KORISNIK*, int, FILE*);
