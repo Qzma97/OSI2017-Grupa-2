33810beeb093c7a0ef8561877c1b2ca80ca2212d
natasatrivuncevic <n.trivuncevic2606@gmail.com>
2017-12-20 00:33:36 +0100

    Dodate su funkcije za pretragu liste po kupcima i po datumu, i dodate su funkcije za pregled podataka za odredjeni artikal i brisanje liste racuna

--- Projekat/Projekat/Pretraga.c
+++ Projekat/Projekat/Pretraga.c
@@ -19,3 +19,76 @@ int trazi_po_artiklu(NODE* head, char* naziv)
 	}
 	return count;
 }
+
+void artikal(NODE* head, int num, char*pom)
+{
+	double ukupno=0 ,kolicina = 0;
+	int index = 0;
+	NODE* temp=head;
+	RACUN* racuni = (RACUN*)calloc(num, sizeof(RACUN));
+	while (num-index)
+	{
+		int i=0,n = temp->racun.brojArtikala;
+		while (n--)
+		{
+			if (strcmp(pom, temp->racun.nizA[i].naziv) == 0)
+			{
+				racuni[index].nizA = &temp->racun.nizA[i];
+				strcpy(racuni[index].datum, temp->racun.datum);
+				ukupno += temp->racun.nizA[i].ukupno;
+				kolicina += temp->racun.nizA[i].kolicina;
+				index++;
+			}
+			i++;
+		}
+		temp = temp->next;
+	}
+	printf("-------------------------------------------------------------------------------------------------\n");
+	printf("%s\n", pom);
+	printf("-------------------------------------------------------------------------------------------------\n");
+	printf("DATUM\t\tKOLICINA\t\tCIJENA\t\tUKUPNO\n");
+	for(int i=0;i<index;i++)
+	{
+		printf("%s\t%.2lf\t\t\t%.2lf\t\t%.2lf\n", racuni[i].datum, racuni[i].nizA->kolicina, racuni[i].nizA->cijena, racuni[i].nizA->ukupno);
+	}
+	printf("-------------------------------------------------------------------------------------------------\n");
+	printf("UKUPNA KOLICINA:%.2lf			UKUPNA CIJENA:%.2lf\n", kolicina, ukupno);
+	free(racuni);
+}
+
+void brisi_listu(NODE**head)
+{
+    NODE*temp=*head;
+    while(temp)
+    {
+        *head=(*head)->next;
+        free(temp->racun.nizA);
+        free(temp);
+        temp=*head;
+    }
+}
+
+
+int trazi_po_kupcu(NODE*head,char*kupac)
+{
+    NODE*p;
+    int br=0;
+    for(p=head;p!=NULL;p=p->next)
+    {
+        if(strcmp(p->racun.kupac,kupac)==0)
+            br++;
+    }
+    return br;
+}
+
+int trazi_po_datumu(NODE*head, char*datum_novi)
+{
+	NODE*p;
+	int br = 0;
+	for (p = head; p != NULL; p = p->next)
+	{
+		if (strcmp(p->racun.datum, datum_novi) == 0)
+			br++;
+	}
+	return br;
+}
--- Projekat/Projekat/Pretraga.h
+++ Projekat/Projekat/Pretraga.h
@@ -4,6 +4,9 @@
 #include "Format4.h"
 
 int trazi_po_artiklu(NODE*, char*);
-
+void artikal(NODE*, int, char*);
+void brisi_listu(NODE**);
+int trazi_po_datumu(NODE*, char*);
+int trazi_po_kupcu(NODE*head, char*kupac);
 
 #endif // FORMAT1_H
